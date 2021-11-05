d719f1db516a0a2dfcad171a69b16f74d76b0791
Nandan2009 <93773684+Nandan2009@users.noreply.github.com>
2021-11-05 17:56:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+prediction_1 = "";
+prediction_2 = "";
+
+Webcam.set({
+    width: 400,
+    height: 400,
+    image_format: "png",
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+}
+
+console.log("ml5 version:",ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/upi5H0rlu/model.json",modelLoaded);
+
+function modelLoaded(){
+    console.log("modelLoaded");
+}
+
+function speak(){
+    var syn = window.speechSynthesis;
+    speak_data_1 = "The First Prediction Is"  + prediction_1;
+    speak_data_2 = "And The Second Prediction Is" + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1+speak_data_2); synth.speak(utterThis);
+}
